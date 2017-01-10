d4c2b599ffa83ff49acf358ed0257b09cd8e32cf
Paul Holser <pholser@alumni.rice.edu>
2017-01-10 12:42:41 -0600

    Add Twilio endpoint

--- src/main/java/com/containerstore/whereis/config/SecurityConfig.java
+++ src/main/java/com/containerstore/whereis/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().anyRequest().permitAll()
+            .and().csrf().ignoringAntMatchers("/incoming");
     }
 }
--- src/main/java/com/containerstore/whereis/controller/WhereIsController.java
+++ src/main/java/com/containerstore/whereis/controller/WhereIsController.java
@@ -1,11 +1,20 @@
 package com.containerstore.whereis.controller;
 
 import com.containerstore.whereis.WhereIsViewModel;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.rest.api.v2010.account.MessageCreator;
+import com.twilio.type.PhoneNumber;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.*;
 
 @Controller
 public class WhereIsController {
@@ -24,6 +33,21 @@ public class WhereIsController {
 
         model.addAttribute("queryModel", viewModel);
         return "whereis";
+    }
+
+    @PostMapping("/incoming")
+    @ResponseStatus(OK)
+    public void incoming(
+            @RequestBody String query,
+            @RequestParam("From") String origin) {
+
+        Twilio.init("(account sid)", "(auth token)");
+
+        MessageCreator creator = Message.creator(
+                new PhoneNumber(origin),
+                new PhoneNumber("4694164526"),
+                replyFor(query));
+        creator.create();
     }
 
     private String replyFor(String query) {
