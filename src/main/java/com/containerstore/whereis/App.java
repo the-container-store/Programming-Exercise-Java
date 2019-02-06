2736dd9b4a7029db14a8e558d7c98770059e3452
Dilip Krishnan <D_KRISHNAN@containerstore.com>
2019-02-06 05:55:39 -0600

    Added servlet initializer to allow application to work in hosted tomcat

--- src/main/java/com/containerstore/whereis/App.java
+++ src/main/java/com/containerstore/whereis/App.java
@@ -2,10 +2,14 @@ package com.containerstore.whereis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
-    public static void main(String[] args) {
+public class App extends SpringBootServletInitializer {
+
+
+  public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
 }
